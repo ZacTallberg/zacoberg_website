58cafef8698d2d6762c98dfcb586bc4a0a0b7dee
zcoberg <zac.oberg@praece.com>
2019-11-13 16:47:04 -0800

    yep

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'testapp'
 ]
 
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #STATIC_ROOT = '/static/'
