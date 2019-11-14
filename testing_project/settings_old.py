4b0eb539d630556e33adf826aaf54335b5561e7c
zcoberg <zac.oberg@praece.com>
2019-11-13 16:06:03 -0800

    test

--- testing_project/settings_old.py
+++ testing_project/settings_old.py
@@ -23,10 +23,55 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zd9n!vf=empkq21e1k-dj$@x2h#)3#hatjoapd6mk1@j#vz*ih'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web", os.getenv("ALLOWED_HOST", "localhost")] if os.getenv("NODEBUG") is None else [".zacoberg.com"]
 
+# if os.environ.get("IN_DOCKER"):
+#     # Stuff for when running in Docker-compose.
+
+#     CELERY_BROKER_URL = 'redis://redis:6379/1'
+#     CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
+
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': "postgres",
+#             'USER': 'postgres',
+#             'PASSWORD': 'password',
+#             'HOST': "db",
+#             'PORT': 5432,
+#         }
+#     }
+# elif os.environ.get("DATABASE_URL"):
+#     # Stuff for when running in Dokku.
+
+#     # Parse the DATABASE_URL env var.
+#     USER, PASSWORD, HOST, PORT, NAME = re.match("^postgres://(?P<username>.*?)\:(?P<password>.*?)\@(?P<host>.*?)\:(?P<port>\d+)\/(?P<db>.*?)$", os.environ.get("DATABASE_URL", "")).groups()
+
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': NAME,
+#             'USER': USER,
+#             'PASSWORD': PASSWORD,
+#             'HOST': HOST,
+#             'PORT': int(PORT),
+#         }
+#     }
+
+#     CELERY_BROKER_URL = os.environ.get("REDIS_URL", "") + "/1"
+#     CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL", "") + "/1"
+# else:
+    # Stuff for when running locally.
+
+# CELERY_TASK_ALWAYS_EAGER = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Application definition
 
@@ -37,6 +82,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp'
 ]
 
 MIDDLEWARE = [
@@ -70,17 +116,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'testing_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -103,18 +138,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'PST'
+# # TIME_ZONE = 'PST'
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
