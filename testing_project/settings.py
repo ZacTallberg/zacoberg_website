7b9ed98a3fff05401d78ec8a2b0c304ff659387f
zcoberg <zac.oberg@praece.com>
2019-11-13 16:21:33 -0800

    removed all bad dependencies

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'PST'
+# TIME_ZONE = 'PST'
 
 USE_I18N = True
 
