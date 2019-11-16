7ebace8b578796bdee4f779519c417de0521ce8f
zcoberg <zac.oberg@praece.com>
2019-11-15 19:56:26 -0800

    refactor

--- testing_project/urls.py
+++ testing_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import settings
+from django.contrib.staticfiles.urls import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('testapp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
