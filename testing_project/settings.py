a8ba4d779c519066a40f48833397aadbc20617bc
zcoberg <zac.oberg@praece.com>
2019-11-15 19:56:26 -0800

    refactor

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp'
+    'testapp.apps.TestappConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 #STATIC_ROOT = '/static/'
