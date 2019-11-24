dc595a52001536e5d78c9d185e3c5f16ddf401bc
zcoberg <zac.oberg@praece.com>
2019-11-23 16:39:59 -0800

    prism

--- testapp/views.py
+++ testapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import views
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, 'index.html')
     #return HttpResponse("Meghan is a Goddess" + " " + "Hail Seitan")
+    
+def prism(request):
+    return render(request, 'prism.html')    
