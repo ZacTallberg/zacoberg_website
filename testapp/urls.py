287dfa35a6653d0dc6a662a72042c3209415286e
zcoberg <zac.oberg@praece.com>
2019-11-27 10:39:23 -0800

    test

--- testapp/urls.py
+++ testapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('prism/', views.prism, name='prism'),
+    path('kitten/', views.Kitten.as_view(), name='kitten'),
 ]
 
 
