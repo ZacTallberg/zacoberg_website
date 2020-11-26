6775f53b70dc4d4f31c69f3dc7e6a91c5a5e00f7
unknown <zcoberg@gmail.com>
2020-11-26 10:34:27 -0800

    untracked .env

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "files")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zd9n!vf=empkq21e1k-dj$@x2h#)3#hatjoapd6mk1@j#vz*ih'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') == 'True')
@@ -40,6 +40,7 @@ if DEBUG:
     ALLOWED_HOSTS.append('localhost')
     ALLOWED_HOSTS.append('127.0.0.1')
 
+DOMAIN = (os.environ.get(ENV) == prod, '.zacoberg.com', '127.0.0.1')
 
 # Application definition
 
