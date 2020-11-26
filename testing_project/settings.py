3c5ab3bc936e96cb093d3cd5cf23af0cb2a33e53
unknown <zcoberg@gmail.com>
2020-11-25 22:16:41 -0800

    ugh

--- testing_project/settings.py
+++ testing_project/settings.py
@@ -30,7 +30,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "files")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'zd9n!vf=empkq21e1k-dj$@x2h#)3#hatjoapd6mk1@j#vz*ih'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') == 'True')
